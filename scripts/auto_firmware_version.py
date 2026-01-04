606553aaea7f068efd9ce343467b64d200d0ff24
Sneeze <sneeze@demented.cc>
2026-01-04 03:33:05 -0800

    Simplify version script for v2.0.0 release

--- scripts/auto_firmware_version.py
+++ scripts/auto_firmware_version.py
@@ -1,28 +1,14 @@
 #!/usr/bin/env python3
 """
-Auto firmware version from git tags
-Adds -D AUTO_VERSION="vX.X.X" build flag
+Auto firmware version
+Adds -D AUTO_VERSION="v2.0.0" build flag
 """
-
-import subprocess
 
 Import("env")
 
-def get_firmware_specifier_build_flag():
-    try:
-        ret = subprocess.run(["git", "describe", "--tags", "--dirty"],
-                           stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-        if ret.returncode == 0:
-            build_version = ret.stdout.strip()
-        else:
-            build_version = "dev"
-    except:
-        build_version = "dev"
-
-    build_flag = '-D AUTO_VERSION=\\"' + build_version + '\\"'
-    print("Firmware Version: " + build_version)
-    return build_flag
+build_version = "v2.0.0"
+print("Firmware Version: " + build_version)
 
 env.Append(
-    BUILD_FLAGS=[get_firmware_specifier_build_flag()]
+    BUILD_FLAGS=['-D AUTO_VERSION="' + build_version + '"']
 )
